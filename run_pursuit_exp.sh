2dbb8779b88c6109ca7e669ceea9979a651846dd
Jacob Deutsch <jacobdeutsch@utexas.edu>
2023-05-23 21:21:22 -0700

    fixes experiment script

--- run_pursuit_exp.sh
+++ run_pursuit_exp.sh
@@ -22,14 +22,14 @@ python pursuit.py -m mlp  --env_size 32 --obs_range 16 --train_batch_size 5000 -
 python pursuit.py -m gat  --env_size 32 --obs_range 16 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
 
 
-python pursuit.py -m mlp  --num_evaders 60 --num_pursuers 8 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
-python pursuit.py -m gat  --num_evaders 60 --num_pursuers 8 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
+python pursuit.py -m mlp  --n_evaders 60 --n_pursuers 8 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
+python pursuit.py -m gat  --n_evaders 60 --n_pursuers 8 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
 
-python pursuit.py -m mlp  --num_evaders 60 --num_pursuers 8 --env_size 32 --obs_range 16 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
-python pursuit.py -m gat  --num_evaders 60 --num_pursuers 8 --env_size 32 --obs_range 16 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
+python pursuit.py -m mlp  --n_evaders 60 --n_pursuers 8 --env_size 32 --obs_range 16 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
+python pursuit.py -m gat  --n_evaders 60 --n_pursuers 8 --env_size 32 --obs_range 16 --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
 
-python pursuit.py -m mlp  --num_evaders 60 --num_pursuers 8 --env_size 32  --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
-python pursuit.py -m gat  --num_evaders 60 --num_pursuers 8 --env_size 32  --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
+python pursuit.py -m mlp  --n_evaders 60 --n_pursuers 8 --env_size 32  --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
+python pursuit.py -m gat  --n_evaders 60 --n_pursuers 8 --env_size 32  --train_batch_size 5000 --lr 0.0001 --batch_mode truncate_episodes --num_workers 3 --use_wandb
 
 
 
