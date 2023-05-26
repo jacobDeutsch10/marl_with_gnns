3818981c8a1e04299445362eeee71bc9a0b22b33
Jacob Deutsch <jacobdeutsch@utexas.edu>
2023-05-25 21:34:50 -0700

    drop batch size

--- run_spread.sh
+++ run_spread.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
-#python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 
-#python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 
 
 
-#python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3
-#python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3
 
-#python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5
-#python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5
-
-
-#python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7
-#python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5
 
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --N 5
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --N 5
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7
 
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 5
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 5
-
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 5
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 5
-
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 5
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 5
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --N 5
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --N 5
 
 
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 5
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 5
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --N 10
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --N 10
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 5
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 5
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 10
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 10
-
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 10
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 10
-
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 10
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 10
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 5
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 5
 
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --N 20
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --N 20
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 20
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 20
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --N 10
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --N 10
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 20
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 20
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 10
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 10
 
-python spread.py -m gnn  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 20
-python spread.py -m mlp  --train_batch_size 5000 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 20
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 10
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 10
+
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 10
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 10
+
+
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --N 20
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --N 20
+
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 20
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.3 --N 20
+
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 20
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.5 --N 20
+
+python spread.py -m gnn  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 20
+python spread.py -m mlp  --train_batch_size 1024 --lr 0.0001 --use_wandb --num_workers 3 --mask_prob 0.7 --N 20
 
 
 
